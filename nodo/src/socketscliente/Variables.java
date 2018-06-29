f241903cb8ebb4872e7ce89c7b768e1585e0533b
Leonardo Azocar <leonardoazocar23@gmail.com>
2018-06-29 16:05:40 -0400

    Subiendo Proyecto 2da parte

---
+++ nodo/src/socketscliente/Variables.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketscliente;
+
+/**
+ *
+ * @author leona
+ */
+public class Variables {
+    
+    public static final String nodo1 = "192.168.1.101";
+    public static final String nodo2 = "127.0.0.2";
+    public static final String nodo3 = "127.0.0.3";
+    public static final String nodo4 = "127.0.0.4";
+    public static final int puerto = 24618;
+    
+}
