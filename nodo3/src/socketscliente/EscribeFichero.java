6bf6fc360322d6ad30934cdace4d39149921477c
Leonardo Azocar <leonardoazocar23@gmail.com>
2018-06-30 13:53:51 -0430

    Arreglando nodo 3

--- nodo3/src/socketscliente/EscribeFichero.java
+++ nodo3/src/socketscliente/EscribeFichero.java
@@ -10,7 +10,7 @@ public class EscribeFichero
         
         try
         {
-            PrintWriter writer = new PrintWriter("stats3.txt", "UTF-8");
+            PrintWriter writer = new PrintWriter("C:\\Users\\GOMEZ\\Downloads\\ProyectoDistribuidos2\\stats3.txt", "UTF-8");
             
             for (int i = 0; i < 3; i++) {
                 writer.println(_lineas[i]);
