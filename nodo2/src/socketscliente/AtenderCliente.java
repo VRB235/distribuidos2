e2cdfbc2e94429e8f8821d8a170900354fbf0434
Unknown <aagilazer_95@hotmail.com>
2018-06-30 03:11:52 -0400

    agregando cosas

--- nodo2/src/socketscliente/AtenderCliente.java
+++ nodo2/src/socketscliente/AtenderCliente.java
@@ -15,8 +15,6 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.rmi.registry.LocateRegistry; 
-import java.rmi.registry.Registry;  
 
 /**
  *
@@ -59,14 +57,8 @@ public class AtenderCliente implements Runnable {
                         _dataOutputStream.writeUTF("ack");
                         _out = new ObjectOutputStream(_socket.getOutputStream());
                         _in = new ObjectInputStream(_socket.getInputStream());
-                        
-                        
-                        Transporte _transporte = (Transporte) _in.readObject();
-                        
-                    /////RMI 
-
-                    /////FIN DE RMI     
-                    
+                          
+                        Transporte _transporte = (Transporte) _in.readObject();                  
                         ArrayList<Paquete> _paquetes = _transporte.getPaquetes();
                         if(_paquetes.size()!=0)
                         {
---
+++ nodo4/src/socketscliente/ConexionRmi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package socketscliente;
+import java.rmi.registry.LocateRegistry; 
+import java.rmi.registry.Registry;  
+import java.io.IOException;
+
+
+/**
+ *
+ * @author Andy
+ */
+public class ConexionRmi {
+    
+//    public void conectar{
+//     try {  
+//         // Getting the registry 
+//         Registry registry = LocateRegistry.getRegistry(null); 
+//    
+//         // Looking up the registry for the remote object 
+//         Hello stub = (Hello) registry.lookup("Hello"); 
+//    
+//         // Calling the remote method using the obtained object 
+//         stub.printMsg(); 
+//         
+//         // System.out.println("Remote method invoked"); 
+//      } catch (Exception e) {
+//         System.err.println("Client exception: " + e.toString()); 
+//         e.printStackTrace(); 
+//      } 
+//}
+    
+}
